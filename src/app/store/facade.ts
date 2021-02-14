6b831fc4505e8a2a7baa478e2bc4b2c72089bdd6
Franklin Okeh <frankok101@gmail.com>
2021-02-14 09:43:08 +0100

    0.1

---
+++ src/app/store/facade.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { select, State, Store } from '@ngrx/store';
+import {  Subject, Observer, Observable } from 'rxjs';
+import { CreditCardState } from '../models/credit-card.interface';
+import { load, payWithCard, payWithCardSuccess } from './actions'
+import { CreditCardQuery } from './selectors';
+
+@Injectable()
+export class CreditCardPaymentFacade {
+ readonly data$: Observable<CreditCardState>;
+
+ constructor(private store: Store) {
+  // @ts-expect-error
+   this.data$ = this.store.pipe(select(CreditCardQuery.getCreditCardState));
+   //this.heroes$ =this.store.pipe(select(fromStore.getAllHeroes));
+ }
+
+  getCreditCardData() {
+    this.store.dispatch(load());
+  }
+
+  makePayment(paymentData: CreditCardState) {
+    this.store.dispatch(payWithCard({paymentData}))
+  }
+
+  storeCard(creditCardData) {
+    this.store.dispatch(payWithCardSuccess(creditCardData))
+  }
+}
